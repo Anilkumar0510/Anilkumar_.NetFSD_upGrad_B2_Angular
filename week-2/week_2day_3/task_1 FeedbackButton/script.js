0dc9986074fb4549bd5ee0abeb268cbc22997653
Anilkumar0510 <akulaanilkumar13@gmail.com>
2026-03-11 23:08:20 +0530

    Delete week-2/week_2day_3 directory

--- week-2/week_2day_3/task_1 FeedbackButton/script.js
+++
@@ -1,5 +0,0 @@
-const btn=document.getElementById("feedbackBtn");
-const msg=document.getElementById("message");
-btn.addEventListener("click",()=>{
-    msg.textContent="Thank you! for submitting your feedback.";
-});
