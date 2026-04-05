c47967bc2a7aaf51cb64fa9b4768fec12471921e
Anilkumar0510 <akulaanilkumar13@gmail.com>
2026-04-05 20:17:11 +0530

    Add files via upload

---
+++ week 6/day 3/ProductInfo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HandsonW6D3
+{
+    internal class ProductInfo
+    {
+    }
+}
